514ef7d65e46fff71b4152b9921ad2e914a0e67d
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2025-08-12 09:26:58 +0700

    add logout info

--- src/features/auth/hooks/use-logout.ts
+++ src/features/auth/hooks/use-logout.ts
@@ -3,14 +3,18 @@
 import { LOCAL_STORAGE_KEYS } from "@/lib/constants";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import { useRouter } from "next/navigation";
+import { useSnackbar } from "notistack";
 import { useEffect } from "react";
 
 export function useLogout() {
   const [token] = useLocalStorage(LOCAL_STORAGE_KEYS.TOKEN, "");
+  const { enqueueSnackbar } = useSnackbar();
   const router = useRouter();
 
   useEffect(() => {
     if (token) return;
+
+    enqueueSnackbar("You have been logged out", { variant: "info" });
 
     router.replace("/login");
   }, [token]);
